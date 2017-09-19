414dca4558fe33f7a528590af0be45b12d2fe58e
Faminchik Evgeniy <f.zhek@mail.ru>
2017-09-20 00:51:25 +0300

    guessing-game.js and .gitignore have been updated

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max
     }
 
     guess() {
-
+        return Math.floor((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess() + 1;
     }
 }
 
-module.exports = GuessingGame;
+
